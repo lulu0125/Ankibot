76f8f10a806b64aff6d1ade012ff7311380b9373
lulu0125 <155187247+lulu0125@users.noreply.github.com>
2025-10-20 19:12:48 +0200

    Delete ankibot/ankibot directory

--- ankibot/ankibot/main.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-from ankibot.ui import AnkibotApp
-
-def main(page: ft.Page):
-    AnkibotApp(page).start()
-
-if __name__ == "__main__":
-    ft.app(target=main)
--- ankibot/ankibot/ui/__init__.py
+++
@@ -1,3 +0,0 @@
-from .app import AnkibotApp
-
-__all__ = ["AnkibotApp"]
